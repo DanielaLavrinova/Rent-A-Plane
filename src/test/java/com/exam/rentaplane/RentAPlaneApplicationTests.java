38d9f55f649c93b4c66a65c773fe31d218f19003
DanielaLavrinova <harila1@icloud.com>
2025-03-11 14:39:33 +0200

    exam project creation

---
+++ src/test/java/com/exam/rentaplane/RentAPlaneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exam.rentaplane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentAPlaneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
